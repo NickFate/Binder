b0aca5fde05a2ab8401f9829a48231c7e9bc4020
Nick Fate <fategraffit@gmail.com>
2022-06-10 22:02:07 +0300

    Основная логика

---
+++ Binder/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Binder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(632, 415);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Склеить";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(551, 415);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Выбрать файлы";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(713, 415);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 2;
+            this.SaveButton.Text = "Сохранить";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "MainForm";
+            this.Text = "Binder";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Binder/MainForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Binder
+{
+    public partial class MainForm : Form
+    {
+        
+
+        private List<Bitmap> images;
+        private int imageWidth, imageHeight, matrixSize;
+        private Bitmap newImage;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+            ImageLoad();
+        }
+
+
+        /// <summary>
+        /// Открывает диалоговое окно с выбором изображений для склейки 
+        /// </summary>
+        private void ImageLoad()
+        {
+
+            images = new List<Bitmap>();
+            OpenFileDialog files = new OpenFileDialog();
+            files.Filter = "Файлы картинок| *.png; *.jpg ";
+
+            // разрешаем выбирать файлы
+            files.Multiselect = true;
+            files.Title = "Выберите изображения для склейки";
+            if (files.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string file in files.FileNames)
+                {
+                    images.Add(new Bitmap(file));
+                }
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveImage();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            BinderFoo();
+        }
+
+        /// <summary>
+        /// Склеивает изображения из <param>images</param>
+        /// </summary>
+        public void BinderFoo()
+        {
+
+            imageWidth = images[0].Width;
+            imageHeight = images[0].Height;
+            matrixSize = images.Count;
+            newImage = new Bitmap(imageWidth * matrixSize, imageHeight * 1);
+            newImage.SetResolution(72, 72);
+           
+            Graphics g = Graphics.FromImage(newImage);
+            int y = 0, count = 0; 
+            for (int x = 0; x < imageWidth * matrixSize; x += imageWidth)
+            {
+                g.DrawImage(images[count], x, y);
+                count++;
+            }
+
+        }
+
+
+       /// <summary>
+       /// Сохраняет изображение
+       /// </summary>
+        private void SaveImage()
+        {
+
+            SaveFileDialog file = new SaveFileDialog();
+            file.Filter = "Png Image|*.png";
+            file.Title = "Сохранить";
+            file.ShowDialog();
+
+            newImage.Save(file.FileName, System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
